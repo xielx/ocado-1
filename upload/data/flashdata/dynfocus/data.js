0621e8685985cb70b0a533782b2b2f57dfc57b77
kinch <jinqiqiqi@gmail.com>
2014-12-20 23:43:47 +0800

    add bottom msg and start to add the banner.

--- upload/data/flashdata/dynfocus/data.js
+++ upload/data/flashdata/dynfocus/data.js
@@ -1,13 +1,10 @@
-imgUrl1="data/afficheimg/20081027angsif.jpg";
-imgtext1="ECShop";
-imgLink1=escape("http://www.ecshop.com");
-imgUrl2="data/afficheimg/20081027wdwd.jpg";
-imgtext2="WDWD";
-imgLink2=escape("http://www.wdwd.com");
-imgUrl3="data/afficheimg/20081027xuorxj.jpg";
-imgtext3="ECShop";
-imgLink3=escape("http://help.ecshop.com/index.php?doc-view-108.htm");
+imgUrl1="data/afficheimg/20141220fiuhbj.jpg";
+imgtext1="";
+imgLink1=escape("http://");
+imgUrl2="data/afficheimg/20141220hiuqop.jpg";
+imgtext2="";
+imgLink2=escape("http://");
 
-var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3;
-var links=imgLink1+"|"+imgLink2+"|"+imgLink3;
-var texts=imgtext1+"|"+imgtext2+"|"+imgtext3;
+var pics=imgUrl1+"|"+imgUrl2;
+var links=imgLink1+"|"+imgLink2;
+var texts=imgtext1+"|"+imgtext2;
