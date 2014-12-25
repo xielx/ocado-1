27b18c4bca2b9e26775737410e89566863b7bd5a
Kinch <jinqiqiqi@gmail>
2014-12-25 21:27:06 +0800

    finish the detail page.

--- upload/includes/lib_insert.php
+++ upload/includes/lib_insert.php
@@ -74,6 +74,7 @@ function insert_query_info()
 function insert_history()
 {
     $str = '';
+    $history_str = array();
     if (!empty($_COOKIE['ECS']['history']))
     {
         $where = db_create_in($_COOKIE['ECS']['history'], 'goods_id');
@@ -89,11 +90,17 @@ function insert_history()
             $goods['goods_thumb'] = get_image_path($row['goods_id'], $row['goods_thumb'], true);
             $goods['shop_price'] = price_format($row['shop_price']);
             $goods['url'] = build_uri('goods', array('gid'=>$row['goods_id']), $row['goods_name']);
-            $str.='<ul class="clearfix"><li class="goodsimg"><a href="'.$goods['url'].'" target="_blank"><img src="'.$goods['goods_thumb'].'" alt="'.$goods['goods_name'].'" class="B_blue" /></a></li><li><a href="'.$goods['url'].'" target="_blank" title="'.$goods['goods_name'].'">'.$goods['short_name'].'</a><br />'.$GLOBALS['_LANG']['shop_price'].'<font class="f1">'.$goods['shop_price'].'</font><br /></li></ul>';
+
+            array_push($history_str, '<div class="col-sm-6 col-md-3">');
+                array_push($history_str, '<div class="event_box event_animate_left"><a href="'.$goods['url'].'" target="_blank"><img src="'.$goods['goods_thumb'].'" alt="" class="img-responsive" /></a>');
+                    array_push($history_str, '<ul>');
+                        array_push($history_str, '<li><a href="'.$goods['url'].'" target="_blank"><span class="title">巧克力黑金砖</span></a><a href="'.$goods['url'].'" target="_blank"><span class="intro">会员价： <span class="price">￥280</span> </span></a></li>');
+                    array_push($history_str, '</ul>');
+                array_push($history_str, '</div>');
+            array_push($history_str, '</div>');
         }
-        $str .= '<ul id="clear_history"><a onclick="clear_history()">' . $GLOBALS['_LANG']['clear_history'] . '</a></ul>';
     }
-    return $str;
+    return implode('', $history_str);
 }
 
 /**
