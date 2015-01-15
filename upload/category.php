8e5a063651363c92c95a1667033e79caa1abb5c7
kinch <jinqiqiqi@gmail.com>
2015-01-15 21:43:57 +0800

    change the color for the tag on the detail page.

--- upload/category.php
+++ upload/category.php
@@ -65,7 +65,7 @@ $default_sort_order_type   = $_CFG['sort_order_type'] == '0' ? 'goods_id' : ($_C
 $sort  = (isset($_REQUEST['sort'])  && in_array(trim(strtolower($_REQUEST['sort'])), array('goods_id', 'shop_price', 'last_update'))) ? trim($_REQUEST['sort'])  : $default_sort_order_type;
 $order = (isset($_REQUEST['order']) && in_array(trim(strtoupper($_REQUEST['order'])), array('ASC', 'DESC')))                              ? trim($_REQUEST['order']) : $default_sort_order_method;
 $display  = (isset($_REQUEST['display']) && in_array(trim(strtolower($_REQUEST['display'])), array('list', 'grid', 'text'))) ? trim($_REQUEST['display'])  : (isset($_COOKIE['ECS']['display']) ? $_COOKIE['ECS']['display'] : $default_display_type);
-$display  = in_array($display, array('list', 'grid', 'text')) ? $display : 'text';
+$display  = in_array($display, array('list', 'grid', 'text')) ? $display : 'list';
 setcookie('ECS[display]', $display, gmtime() + 86400 * 7);
 /*------------------------------------------------------ */
 //-- PROCESSOR
